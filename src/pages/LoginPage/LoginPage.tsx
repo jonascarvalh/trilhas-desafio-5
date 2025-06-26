98396d380ed4665bfe8da939532f2c2637a4b7c6
ValeriaNascimento17 <valeriasoussa91@gmail.com>
2025-06-25 22:58:31 -0300

    Validação dos campos de login e registro

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,40 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import styles from './LoginPage.module.css';
 
 const LoginPage: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [erro, setErro] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!email.trim() || !senha.trim()) {
+      setErro('Por favor, preencha todos os campos.');
+      return;
+    }
+
+    
+  };
+
   return (
     <div>
       <Header />
       <div className={styles.loginPage}>
         <div className={styles.loginContainer}>
           <h1 className={styles.loginTitle}>Entrar</h1>
-          <form className={styles.loginForm}>
-
+          <form className={styles.loginForm} onSubmit={handleSubmit}>
             <div className={styles.loginInput}>
               <p className={styles.loginLabel}>E-mail:</p>
-              <input type="email" placeholder='Digite seu e-mail' />
-            </div>
-            
-            <div className={styles.loginInput}>
-              <p className={styles.loginLabel}>Senha:</p>
-              <input type="password" placeholder='Digite sua senha' />
+              <input
+                type="email"
+                placeholder="Digite seu e-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
 
+            <div className={styles.loginInput}>
+              <p className={styles.loginLabel}>Senha:</p>
+              <input
+                type="password"
+                placeholder="Digite sua senha"
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
+              />
+            </div>
+
+{erro && <p className={styles.loginError}>{erro}</p>}
+
+
             <div className={styles.loginActions}>
-              <button className={styles.loginButton}>Fazer Login</button>
-              <p className={styles.helpText}>Não possui conta? 
+              <button className={styles.loginButton} type="submit">
+                Fazer Login
+              </button>
+              <p className={styles.helpText}>
+                Não possui conta?{' '}
                 <Link to="/register" className={styles.helpTextLink}>
                   <strong> Registre-se aqui</strong>
                 </Link>
               </p>
             </div>
-
           </form>
         </div>
-
       </div>
       <Footer />
     </div>
