dfa0a4315d064a60d8b1855eecc9f1b68766ee6f
jonascarvalh <olaffic@gmail.com>
2025-06-30 22:29:53 -0300

    fix: adicionando tratamento de cors com proxy para o target correto

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:3000',
+        target: 'https://back-end-conectaai-production.up.railway.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
